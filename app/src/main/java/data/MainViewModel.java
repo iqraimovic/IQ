30ef8235c5acb404e1b79621a3470fe665509751
Iqram Olorode <Olor0008@algonquinlive.com>
2023-02-08 14:38:10 -0500

    Last changes to week2

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -2,9 +2,19 @@ package data;
 
 import androidx.lifecycle.MutableLiveData;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
+
+import ui.MainActivity;
 
 public class MainViewModel extends ViewModel {
     public MutableLiveData<String> editString = new MutableLiveData<>();
+
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>(false);
+    //public Object isSelected;
+    public MutableLiveData<String> iButton = new MutableLiveData<>();
+
+    public void observe(MainActivity mainActivity, Observer<Boolean> booleanObserver) {
+    }
 
 }
--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.iqramsandriodlab.R;
 import com.example.iqramsandriodlab.databinding.ActivityMainBinding;
@@ -17,6 +21,7 @@ import data.MainViewModel;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
     private MainViewModel model;
+
 
 
     @Override
@@ -35,10 +40,51 @@ public class MainActivity extends AppCompatActivity {
         });
 
         model.editString.observe(this, s -> variableBinding.textview.setText("Your edit text has " + s));
+
         
         TextView mytext = findViewById(R.id.textview);
         EditText myedit = findViewById(R.id.myedittext);
         myedit.getText().toString();
+
+        model.isSelected.observe(this, selected ->{
+           variableBinding.mybox.setChecked(selected);
+            variableBinding.myradio.setChecked(selected);
+            variableBinding.myswitch.setChecked(selected);
+            Context context = getApplicationContext();
+            CharSequence text = "Your Value is: " + variableBinding.myswitch.isChecked();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+
+        variableBinding.mybox.setOnCheckedChangeListener((checkBox, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.myswitch.setOnCheckedChangeListener((Switch, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+        });
+        variableBinding.myradio.setOnCheckedChangeListener((RadioButton, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+        });
+
+        variableBinding.imageButton.setOnClickListener(click -> {
+            model.iButton.postValue(variableBinding.imageButton.getHeight() + "*" + variableBinding);
+        });
+
+        model.iButton.observe(this, c -> {
+            Context context = getApplicationContext();
+            CharSequence text = "The Value is now: " + variableBinding.imageButton.getHeight();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+        model.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isChecked) {
+                Toast.makeText(getApplicationContext(), "The value is now: " + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
@@ -50,5 +96,8 @@ public class MainActivity extends AppCompatActivity {
        // mybutton.setOnClickListener( vw-> mytext.setText("Your edit has: " + editString));
 
 
+    }
+
+    private void setOnCheckedChangeListener(Object o) {
     }
 }
