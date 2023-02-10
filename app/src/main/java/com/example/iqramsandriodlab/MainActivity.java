dfbf009ace3e9395610427c5ffa071c1bbc38348
Iqram Olorode <Olor0008@algonquinlive.com>
2023-02-10 13:19:43 -0500

    Finished Phone version of lab3

--- app/src/main/java/com/example/iqramsandriodlab/MainActivity.java
+++ app/src/main/java/com/example/iqramsandriodlab/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.iqramsandriodlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        ImageView imgView=findViewById(R.id.imageView);
+        Switch sw = findViewById(R.id.switch1);
+
+
+            sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+                if (isChecked) {
+                    RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setDuration(5000);
+                    rotate.setRepeatCount(Animation.INFINITE);
+                    rotate.setInterpolator(new LinearInterpolator());
+
+                    imgView.startAnimation(rotate);
+                }else{
+                    imgView.clearAnimation();
+                }
+
+            });
+        }
+
 }
