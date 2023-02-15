af305fa6d510ef1463d147f38f7405849cf53868
Iqram Olorode <Olor0008@algonquinlive.com>
2023-02-15 16:10:41 -0500

    Lab4 b4ginning

--- app/src/main/java/com/example/iqramsandriodlab/MainActivity.java
+++ app/src/main/java/com/example/iqramsandriodlab/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.iqramsandriodlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In OnCreate() - Loading Widgets");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In OnResume() - Application is now visible ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In OnPause() - Application no longer responds");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "In OnStop() - Application no longer responds");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "In OnDestroy() - Application memory freed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "In OnStart() - Application is now visible");
     }
 }
